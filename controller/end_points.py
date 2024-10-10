31a6ce804eedac03df87fa374fba349d2a5abceb
ari oren <arioren100@gmail.com>
2024-10-10 14:08:26 +0300

    Third end point

--- controller/end_points.py
+++ controller/end_points.py
@@ -2,7 +2,7 @@ from dataclasses import asdict
 
 from flask import Blueprint, request, jsonify
 
-from repository.accident_repo import count_by_region
+from repository.accident_repo import count_by_region, count_by_region_day, count_by_region_week, count_by_region_month
 from repository.csv_repository import init_car_accident
 
 end_points = Blueprint('end_points', __name__)
@@ -19,11 +19,22 @@ def region(region):
     res = count_by_region(region)
     return jsonify({region: res})
 
-# #Total accidents by region and by period
-# @end_points.route('/region_period/<string:region>/<string:period>', methods=['GET'])
-#
+#Total accidents by region and by period
+@end_points.route('/region_period/<string:type>/<string:region>', methods=['GET'])
+def region_period(type, region):
+    period = request.json
+    res = None
+    if type == 'day':
+        res = count_by_region_day(region, period)
+    elif type == 'week':
+        res = count_by_region_week(region, period)
+    elif type == 'month':
+        res = count_by_region_month(region, period)
+
+    return jsonify({'region_period': res})
+
 # # Accidents are grouped according to the main cause of the accident:
 # @end_points.route('/main_cause/<string:main_cause', methods=['GET'])
 #
-# # Statistics on injuries by region:
+# Statistics on injuries by region:
 # @end_points.route('/injuries/<string:region>', methods=['GET'])
--- repository/accident_repo.py
+++ repository/accident_repo.py
@@ -1,5 +1,24 @@
-from database.connect import my_collection
+from datetime import datetime, timedelta, date
+
+from database.connect import my_collection, days_sum, month_sum
 
 
 def count_by_region(region):
     return my_collection.count_documents({'region': region})
+
+def count_by_region_day(region, period):
+    period = datetime(year=period['year'], month=period['month'], day=period['day'])
+    res = days_sum.find_one({'region': region, 'date': period})
+    return 0 if res is None else res['count']
+
+def count_by_region_week(region, period):
+    start = datetime(year=period['year'], month=period['month'], day=period['day'])
+    end = start + timedelta(days=6)
+    try:
+        return sum(x['count'] for x in list(days_sum.find({'region': region, 'date': {'$gte': start, '$lte': end}})))
+    except Exception:
+        return 0
+
+def count_by_region_month(region, period):
+    res = month_sum.find_one({'region': region, 'date':period})
+    return 0 if res is None else res['count']
